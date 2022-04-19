38d07004aaf532d8e7fe78d24492dc98c4f0cb2c
Miqdad Abdurrohman <miqdadabdurrohman96@gmail.com>
2022-04-19 22:07:23 +0700

    create model and relasi done

---
+++ migrations/20220419141208-create-profile.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Profiles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      bio: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      quote: {
+        type: Sequelize.TEXT,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: { tableName: "Users" },
+          key: "id",
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Profiles");
+  },
+};
